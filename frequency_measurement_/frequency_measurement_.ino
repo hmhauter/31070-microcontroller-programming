53da84aa612731590295dd8abe898d07cc1193c3
helena <helenam.hauter@gmail.com>
2022-06-11 15:57:03 +0200

    RMS calculation, power calculation

--- frequency_measurement_/frequency_measurement_.ino
+++ frequency_measurement_/frequency_measurement_.ino
@@ -1,5 +1,6 @@
 #include "Timer5.h"
 #include <LiquidCrystal.h>
+#include <math.h>
 #define PI 3.1415926535897932384626433832795
 LiquidCrystal lcd(12, 4, 5, 11, 3, 2);
 
@@ -10,12 +11,14 @@ const int ledPin = 1;
 
 // Frequency Calculation
 volatile long int zeroCrossing = 0;
-volatile float sampleCount = 0.0;
+
 float sample = 0.0;
 volatile float frequency = 0.0;
 volatile float debug =0.0;
 const float samplingRate = 10000.0;
-const float offsetValueZeroCrossing = 257.12;
+const float offsetValueZeroCrossing = 1100; // 257.12;
+
+const int resolution = 4095; 
 
 // Low Pass Filter
 const float crossOverFreq = 50.0;
@@ -33,29 +36,55 @@ float oneMinusAlpha = 1.0-alpha; //Calculation to speed up filter calculation in
 volatile float oldY = 0.0;
 volatile float newY = 0.0;
 
+volatile float newT = 0.0;
+volatile float oldT = 0.0;
+
+volatile float voltage = 0.0;
+volatile float ampere = 0.0;
+volatile float RMS_voltage = 0.0;
+volatile float RMS_ampere = 0.0;
+volatile float sampleCounter = 0.0;
+volatile float power = 0.0;
+
 void setup() {
   // put your setup code here, to run once:
-  // Serial.begin(9600);
+  Serial.begin(9600);
   // alpha = calculateAlpha(crossOverFreq, (float)(1.0/samplingRate));
   pinMode(ledPin, OUTPUT);
   pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
   analogWrite(7, 1);
+
   MyTimer5.begin(samplingRate);
   MyTimer5.attachInterrupt(calculateFrequency);
   AdcBooster();
-  DAC->CTRLA.bit.ENABLE = 1; // Enable DAC
+  // DAC->CTRLA.bit.ENABLE = 1; // Enable DAC
   MyTimer5.start();
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
-  lcd.print("HALLO");
 }
 
 void loop() {
+  Serial.println("Frequency: ");
+  Serial.println(frequency, 6);
+  Serial.println("RMS Ampere: ");
+  Serial.println(RMS_ampere, 6);
+  Serial.println("RMS Voltage: ");
+  Serial.println(RMS_voltage, 6);
+    Serial.println("Power: ");
+  Serial.println(power, 6);
     digitalWrite(ledPin, 1);
     if (zeroCrossing % 20 == 0) {
-      // lcd.clear();
-      // lcd.print((sampleCount), 6);
+      lcd.clear();
+      lcd.print((frequency), 6);
       }
+    if (frequency <= 49.9 || frequency >= 50.2 ) {
+        digitalWrite(6, 1);
+        }
+        else {
+          digitalWrite(6,0);
+          }
+
   // put your main code here, to run repeatedly:
   // int sample = analogRead(A1);
   // float volt = (3.3/1023) * sample;
@@ -71,7 +100,7 @@ void AdcBooster() {
   ADC->CTRLA.bit.ENABLE = 0; // Disable ADC
   while( ADC->STATUS.bit.SYNCBUSY == 1 ); // Wait for synchronization
   ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV16 | // Divide Clock by 16.
-  ADC_CTRLB_RESSEL_10BIT; // Result on 10 bits
+  ADC_CTRLB_RESSEL_12BIT; // Result on 10 bits
   ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 | // 1 sample
   ADC_AVGCTRL_ADJRES(0x00ul); // Adjusting result by 0
   ADC->SAMPCTRL.reg = 0x00; // Sampling Time Length = 0
@@ -86,27 +115,38 @@ float lowPassFilter(float Input, float yOld){
 
 void calculateFrequency(void) {
   // Interrupt start
-
   // Calculate Frequency
-  analogReadResolution(10);
-  analogWriteResolution(10);
+  analogReadResolution(12);
+  analogWriteResolution(12);
   sample = (float)analogRead(A1);
   newY = lowPassFilter(sample, oldY);
-
+  
   // detect "zero crossing"
   if(newY >= offsetValueZeroCrossing and oldY < offsetValueZeroCrossing) {
-    if(zeroCrossing % 20 == 0) {
-      lcd.clear();
-      lcd.print((sampleCount), 6);
-      }
-
-    detectZeroCrossing();
+    // detectZeroCrossing();
+    newT = millis()/1000.0;
+    debug = ((newY - offsetValueZeroCrossing) / (newY - oldY)) * (1.0/samplingRate);
+    float period = newT - oldT - debug;
+    frequency = (1.0/period);
+    oldT = newT;
     zeroCrossing++;
-    sampleCount = 0.0;
+    RMS_voltage = sqrt((1/sampleCounter) * voltage);
+    RMS_ampere = sqrt((1/sampleCounter) * ampere);
+    power = RMS_voltage * RMS_ampere;
+    voltage = 0.0;
+    ampere = 0.0;
+    sampleCounter = 0.0;
     }
     else {
-  sampleCount++;
-    }
+//      Serial.println("Sample: ");
+//      Serial.println(sample);
+//      Serial.println("Mapping Volt: ");
+//      Serial.println(map(sample, 0, resolution, -240*sqrt(2), 240*sqrt(2)));
+      voltage = voltage + powf(map(sample, 0, resolution, -240*sqrt(2), 240*sqrt(2)), 2);
+      ampere = ampere + powf(map(sample, 0, resolution, -42*sqrt(2), 42*sqrt(2)), 2);
+      sampleCounter++;
+      
+      }
   oldY = newY;
   analogWrite(A0, newY);
     
@@ -115,25 +155,30 @@ void calculateFrequency(void) {
 void detectZeroCrossing() {
   // debug = ((newY*(1.0/samplingRate))/(newY-oldY));
   // debug = (offsetValueZeroCrossing - oldY) * ((1.0/samplingRate)/(newY-oldY));
-  // debug = ((newY - offsetValueZeroCrossing) / (newY - oldY)) * (1.0/samplingRate);
+  debug = ((newY - offsetValueZeroCrossing) / (newY - oldY)) * (1.0/samplingRate);
   
   // debug = ((newY - offsetValueZeroCrossing) *((1.0/samplingRate)/(newY-oldY)));
    // float period = (sampleCount/samplingRate)-((newY*(1.0/samplingRate))/(newY-oldY));
-   float period = (sampleCount/samplingRate); // -debug;
-//   Serial.println("Plain Period: ");
-//   Serial.println((sampleCount/samplingRate));
+   newT = millis()/1000.0;
+   
+   float period = newT - oldT; //- debug;
+//Serial.println("New T : ");
+//Serial.println(newT);
+//Serial.println("Old T : ");
+//Serial.println(oldT);
+// Serial.println("Debug  : ");
+// Serial.println(debug, 6);
 //  Serial.println("Interpol: ");
 //  Serial.println(((newY*(1/samplingRate))/(newY-oldY)), 5);
-//  Serial.println("Period: ");
-//  Serial.println(period);
+// Serial.println("Period: ");
+// Serial.println(period, 6);
 //    Serial.println("Y new: ");
 //  Serial.println(newY);
 //    Serial.println("Y old: ");
 //  Serial.println(oldY);
   frequency = ((float) 1.0/period);
-  
+  oldT = newT;
 //  Serial.println(zeroCrossing);
-//  Serial.println(sampleCount);
 //  Serial.println(samplingRate);
 //  Serial.println(frequency);
   }
